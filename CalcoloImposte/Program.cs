21ed82ebcf653383a8ad4d77b967a2fa9ba9a551
MethaKinde <meethabeelkinde@gmail.com>
2024-02-02 11:17:42 +0100

    Aggiungere i file di progetto.

---
+++ CalcoloImposte/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalcoloImposte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
